7794e9b8e59707e35f22d287142c345dd8699135
Julio Santos <julio@morgane.com>
2023-02-17 14:23:27 -0600

    fix second count bug by stopping the stopwatch

--- lib/bpm_calculator.dart
+++ lib/bpm_calculator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'draw_circle.dart';
 
 class BPMCalculator extends StatefulWidget {
   BPMCalculator({Key? key, required this.title}) : super(key: key);
@@ -10,18 +11,9 @@ class BPMCalculator extends StatefulWidget {
 
 class _BPMCalculator extends State<BPMCalculator> {
   int _n_taps = 0;
-  int _m_taps = 0;
   int _bpm = 0;
-  int _bpm_old_max = 0;
-  int _bpm_max = 0;
   
-  int _count = 0;
-  List<int> _tapIntervals = [];
-
   Stopwatch _stopwatch = new Stopwatch();
-
-  // TODO
-  // - auto reset after 3 seconds
 
   void _increment() {
     setState(() {
@@ -29,40 +21,20 @@ class _BPMCalculator extends State<BPMCalculator> {
     });
   }
 
-  // void _tap_new() {
-  //   final tapInterval = _stopwatch.elapsedMilliseconds;
-  //   _tapIntervals.add(tapInterval);
-  //   _stopwatch.reset();
-
-  //   // Calculate the average interval between taps
-  //   final averageInterval = _tapIntervals.reduce((a, b) => a + b) ~/ _tapIntervals.length;
-
-  //   // Calculate the beats per minute
-  //   _bpm = 60000 ~/ averageInterval;
-  // }
-
   void _tap() {
-    if (!_stopwatch.isRunning) {
-      _stopwatch.start();
-    } else {
-      _bpm = _n_taps ~/ (_stopwatch.elapsedMicroseconds / 60000000);
-    }
-    if (_bpm_max < _bpm) {
-      _bpm_max = _bpm;
-    }
+    _stopwatch.start();
+      double seconds = _stopwatch.elapsedMicroseconds / 60000000;
+      _bpm = _n_taps ~/ seconds;
+      // print('seconds = $seconds | bpm = $_bpm | _n_taps = $_n_taps');
     _n_taps++;
   }
 
   void _reset() {
     setState(() {
-      _count = 0;
-
-      _m_taps = _n_taps;
       _n_taps = 0;
-      _bpm_old_max = _bpm_max;
       _bpm = 0;
-      _bpm_max = 0;
       _stopwatch.reset();
+      _stopwatch.stop();
     });
   }
 
@@ -118,26 +90,5 @@ class _BPMCalculator extends State<BPMCalculator> {
         backgroundColor: Colors.green,
       ),
     );
-  }
-}
-
-class DrawCircle extends CustomPainter {
-  late Paint _paint;
-
-  DrawCircle() {
-    _paint = Paint()
-      ..color = Color.fromRGBO(92, 184, 92, 0.2)
-      ..strokeWidth = 10.0
-      ..style = PaintingStyle.fill;
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(Offset(0.0, 0.0), 120.0, _paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
---
+++ lib/draw_circle.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DrawCircle extends CustomPainter {
+  late Paint _paint;
+
+  DrawCircle() {
+    _paint = Paint()
+      ..color = Color.fromRGBO(92, 184, 92, 0.2)
+      ..strokeWidth = 10.0
+      ..style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(Offset(0.0, 0.0), 120.0, _paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
